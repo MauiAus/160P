2c87834f26e8b59e2c708db9ec610a3c7264eca0
Mark <markaustin017@gmail.com>
2020-12-09 19:27:55 +0800

    Added SecondPattern

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@ int LED[] = {2,3,4,5,6,7,8,9};
 int numLeds = 8;
 int ctr = 0;
 
+void firstPattern();
+void secondPattern();
+
 void setup() {
   // put your setup code here, to run once:
   for(int i = 0; i < numLeds; i++)
@@ -15,6 +18,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //firstPattern();
+  secondPattern();
+}
+
+void firstPattern() {
   if(ctr == 8)
     ctr = 0;
   digitalWrite(LED[ctr],HIGH);
@@ -24,3 +32,17 @@ void loop() {
   Serial.println(LED[ctr]);
   ctr++;
 }
+
+void secondPattern(){
+  for(int i = 0; i < numLeds; i++){
+    digitalWrite(LED[i],HIGH);
+    delay(250);
+    digitalWrite(LED[i],LOW);
+  }
+  for(int i = numLeds - 1; i >= 0; i--){
+    digitalWrite(LED[i],HIGH);
+    delay(250);
+    digitalWrite(LED[i],LOW);
+  }
+}
+
