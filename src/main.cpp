806ae41c9620ababf3ba1cd0a3d9592ee21b9a15
Mark <markaustin017@gmail.com>
2020-12-09 19:57:42 +0800

    Added pattern three

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int ctr = 0;
 
 void firstPattern();
 void secondPattern();
+void thirdPattern();
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,7 +20,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   //firstPattern();
-  secondPattern();
+  thirdPattern();
 }
 
 void firstPattern() {
@@ -46,3 +47,33 @@ void secondPattern(){
   }
 }
 
+void thirdPattern(){
+  for(int i = 0; i < 25; i++){
+    if(i % 2 == 0)
+      digitalWrite(LED[0],HIGH);
+    else
+      digitalWrite(LED[1],HIGH);
+    delay(100);
+    digitalWrite(LED[0],LOW);
+    digitalWrite(LED[1],LOW);
+  }
+  for(int i = 1; i < numLeds; i++){
+    digitalWrite(LED[i],HIGH);
+    delay(50);
+    digitalWrite(LED[i],LOW);
+  }
+  for(int i = 0; i < 25; i++){
+    if(i % 2 == 0)
+      digitalWrite(LED[numLeds - 1],HIGH);
+    else
+      digitalWrite(LED[numLeds - 2],HIGH);
+    delay(100);
+    digitalWrite(LED[numLeds - 1],LOW);
+    digitalWrite(LED[numLeds - 2],LOW);
+  }
+  for(int i = numLeds - 1; i >= 0; i--){
+    digitalWrite(LED[i],HIGH);
+    delay(50);
+    digitalWrite(LED[i],LOW);
+  }
+}
