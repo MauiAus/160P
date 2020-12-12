6395f765f9c42ecf41ac2680462381873e5da6c9
Mark <markaustin017@gmail.com>
2020-12-13 05:01:46 +0800

    Binary counter works

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,61 @@
 #include <Arduino.h>
 
+int LED[] = {7,8,9,10};
+int inPin[] = {2,3,4,5,6};
+
+int SW1 = 0;
+int SW2 = 0;
+int SW3 = 0;
+int SW4 = 0;
+int SW5 = 0;
+
+int BCD = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  for(int i = 0; i < 4; i++){
+    pinMode(LED[i],OUTPUT);
+  }
+  for(int i = 0; i < 5; i++){
+    pinMode(inPin[i],INPUT);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  SW1 = digitalRead(inPin[0]);
+  SW2 = digitalRead(inPin[1]);
+  SW3 = digitalRead(inPin[2]);
+  SW4 = digitalRead(inPin[3]);
+  SW5 = digitalRead(inPin[4]);
+
+  if(SW1 == HIGH){
+    delay(1000);
+    if(BCD < 9){
+      BCD++;
+    }
+  }
+  if(SW2 == HIGH){
+    delay(1000);
+    if(BCD > -1){
+      BCD--;
+      for(int i = 0; i < 4; i++)
+        digitalWrite(LED[i],LOW);
+    }
+  }  
+  int a[4];
+  int n = BCD; 
+  for(int i=0; n>0; i++)    
+  {
+    a[i]=n%2;    
+    n = n/2;  
+    if(a[i] == 1){
+      digitalWrite(LED[i],HIGH);
+    }
+    else{
+      digitalWrite(LED[i],LOW);
+    }
+    Serial.println(BCD);
+  }    
 }
