50ba2c2525e475ad2b994d6db385c52c1f186fe2
Mark <markaustin017@gmail.com>
2020-12-13 15:17:14 +0800

    Added Grey Code Conv

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@ int SW5 = 0;
 
 int BCD = 0;
 
+int greyConv(int x);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -58,4 +60,52 @@ void loop() {
     }
     Serial.println(BCD);
   }    
+}
+
+int greyConv(int x){
+  switch (x)
+  {
+  case 0:
+    return 0;
+    break;
+  
+  case 1:
+    return 1;
+    break;
+  
+  case 2:
+    return 3;
+    break;
+  
+  case 3:
+    return 2;
+    break;
+  
+  case 4:
+    return 6;
+    break;
+  
+  case 5:
+    return 7;
+    break;
+  
+  case 6:
+    return 5;
+    break;
+  
+  case 7:
+    return 8;
+    break;
+  
+  case 8:
+    return 12;
+    break;
+  
+  case 9:
+    return 13;
+    break;
+  
+  default:
+    break;
+  }
 }
