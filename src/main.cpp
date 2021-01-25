eb89c74d853923a70ff7e38cca09c5a35650fd25
unknown <markaustin017@gmail.com>
2021-01-26 01:29:46 +0800

    Added SSD function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include <Arduino.h>
 
+int sw = 0;//button
+//SSD pins
+int pinA = 0;
+int pinB = 0;
+int pinC = 0;
+int pinD = 0;
+int pinE = 0;
+int pinF = 0;
+int pinG = 0;
+//Stepper pins
+int stp1 = 10;
+int stp2 = 11;
+int stp3 = 12;
+int stp4 = 13;
+
+int stp_ctr = 0;//stepper counter
+int park_ctr = 0;//Parking counter
+
 void setup() {
   // put your setup code here, to run once:
 }
@@ -7,3 +25,111 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
+//function to create one step
+//function to display the SSD
+void displaySSD(int ctr){
+  switch (ctr)
+  {
+  case 0:
+    digitalWrite(pinA, HIGH);   
+    digitalWrite(pinB, HIGH);   
+    digitalWrite(pinC, HIGH);   
+    digitalWrite(pinD, HIGH);   
+    digitalWrite(pinE, HIGH);   
+    digitalWrite(pinF, HIGH);   
+    digitalWrite(pinG, LOW);
+    break;
+  case 1:
+    digitalWrite(pinA, LOW);   
+    digitalWrite(pinB, HIGH);   
+    digitalWrite(pinC, HIGH);   
+    digitalWrite(pinD, LOW);   
+    digitalWrite(pinE, LOW);   
+    digitalWrite(pinF, LOW);   
+    digitalWrite(pinG, LOW);
+    break;
+  case 2:
+    digitalWrite(pinA, HIGH);   
+    digitalWrite(pinB, HIGH);   
+    digitalWrite(pinC, LOW);   
+    digitalWrite(pinD, HIGH);   
+    digitalWrite(pinE, HIGH);   
+    digitalWrite(pinF, LOW);   
+    digitalWrite(pinG, HIGH);
+    break;
+  case 3:
+    digitalWrite(pinA, HIGH);   
+    digitalWrite(pinB, HIGH);   
+    digitalWrite(pinC, HIGH);   
+    digitalWrite(pinD, HIGH);   
+    digitalWrite(pinE, LOW);   
+    digitalWrite(pinF, LOW);   
+    digitalWrite(pinG, HIGH);
+    break;
+  case 4:
+    digitalWrite(pinA, LOW);   
+    digitalWrite(pinB, HIGH);   
+    digitalWrite(pinC, HIGH);   
+    digitalWrite(pinD, LOW);   
+    digitalWrite(pinE, LOW);   
+    digitalWrite(pinF, HIGH);   
+    digitalWrite(pinG, HIGH); 
+    break;
+  case 5:
+    digitalWrite(pinA, HIGH);   
+    digitalWrite(pinB, LOW);   
+    digitalWrite(pinC, HIGH);   
+    digitalWrite(pinD, HIGH);   
+    digitalWrite(pinE, LOW);   
+    digitalWrite(pinF, HIGH);   
+    digitalWrite(pinG, HIGH); 
+    break;
+  case 6:
+    digitalWrite(pinA, LOW);   
+    digitalWrite(pinB, LOW);   
+    digitalWrite(pinC, HIGH);   
+    digitalWrite(pinD, HIGH);   
+    digitalWrite(pinE, HIGH);   
+    digitalWrite(pinF, HIGH);   
+    digitalWrite(pinG, HIGH); 
+    break;
+  case 7:
+    digitalWrite(pinA, HIGH);   
+    digitalWrite(pinB, HIGH);   
+    digitalWrite(pinC, HIGH);   
+    digitalWrite(pinD, LOW);   
+    digitalWrite(pinE, LOW);   
+    digitalWrite(pinF, LOW);   
+    digitalWrite(pinG, LOW); 
+    break;
+  case 8:
+    digitalWrite(pinA, HIGH);   
+    digitalWrite(pinB, HIGH);   
+    digitalWrite(pinC, HIGH);   
+    digitalWrite(pinD, HIGH);   
+    digitalWrite(pinE, HIGH);   
+    digitalWrite(pinF, HIGH);   
+    digitalWrite(pinG, HIGH); 
+    break;
+  case 9:
+    digitalWrite(pinA, HIGH);   
+    digitalWrite(pinB, HIGH);   
+    digitalWrite(pinC, HIGH);   
+    digitalWrite(pinD, LOW);   
+    digitalWrite(pinE, LOW);   
+    digitalWrite(pinF, HIGH);   
+    digitalWrite(pinG, HIGH); 
+    break;
+  default:
+    digitalWrite(pinA, HIGH);   
+    digitalWrite(pinB, HIGH);   
+    digitalWrite(pinC, HIGH);   
+    digitalWrite(pinD, HIGH);   
+    digitalWrite(pinE, HIGH);   
+    digitalWrite(pinF, HIGH);   
+    digitalWrite(pinG, LOW);
+    break;
+  }
+}
+//function to check if the parking is full
