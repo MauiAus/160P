d5d78af7984ff03cccb9f1642c811eaacca1d47c
unknown <markaustin017@gmail.com>
2021-01-26 01:32:29 +0800

    Added Stepper function

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,15 @@ int pinE = 0;
 int pinF = 0;
 int pinG = 0;
 //Stepper pins
-int stp1 = 10;
-int stp2 = 11;
-int stp3 = 12;
-int stp4 = 13;
+int stp_pin1 = 10;
+int stp_pin2 = 11;
+int stp_pin3 = 12;
+int stp_pin4 = 13;
 
 int stp_ctr = 0;//stepper counter
 int park_ctr = 0;//Parking counter
+
+void displaySSD(int ctr);
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,6 +29,68 @@ void loop() {
 }
 
 //function to create one step
+void OneStep(bool dir){
+  if(dir){
+    switch(stp_ctr){
+      case 0:
+        digitalWrite(stp_pin1, HIGH);
+        digitalWrite(stp_pin2, LOW);
+        digitalWrite(stp_pin3, LOW);
+        digitalWrite(stp_pin4, LOW);
+        break;
+      case 1:
+        digitalWrite(stp_pin1, LOW);
+        digitalWrite(stp_pin2, HIGH);
+        digitalWrite(stp_pin3, LOW);
+        digitalWrite(stp_pin4, LOW);
+        break;
+      case 2:
+        digitalWrite(stp_pin1, LOW);
+        digitalWrite(stp_pin2, LOW);
+        digitalWrite(stp_pin3, HIGH);
+        digitalWrite(stp_pin4, LOW);
+        break;
+      case 3:
+        digitalWrite(stp_pin1, LOW);
+        digitalWrite(stp_pin2, LOW);
+        digitalWrite(stp_pin3, LOW);
+        digitalWrite(stp_pin4, HIGH);
+        break;
+    }
+  }
+    else{
+      switch(stp_ctr){
+        case 0:
+        digitalWrite(stp_pin1, LOW);
+        digitalWrite(stp_pin2, LOW);
+        digitalWrite(stp_pin3, LOW);
+        digitalWrite(stp_pin4, HIGH);
+        break;
+        case 1:
+        digitalWrite(stp_pin1, LOW);
+        digitalWrite(stp_pin2, LOW);
+        digitalWrite(stp_pin3, HIGH);
+        digitalWrite(stp_pin4, LOW);
+        break;
+        case 2:
+        digitalWrite(stp_pin1, LOW);
+        digitalWrite(stp_pin2, HIGH);
+        digitalWrite(stp_pin3, LOW);
+        digitalWrite(stp_pin4, LOW);
+        break;
+        case 3:
+        digitalWrite(stp_pin1, HIGH);
+        digitalWrite(stp_pin2, LOW);
+        digitalWrite(stp_pin3, LOW);
+        digitalWrite(stp_pin4, LOW);
+        break;
+      }
+    }
+  stp_ctr++;
+  if(stp_ctr>3)
+    stp_ctr = 0;
+}
+
 //function to display the SSD
 void displaySSD(int ctr){
   switch (ctr)
